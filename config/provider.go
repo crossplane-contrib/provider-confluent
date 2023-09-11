946d386df1abd753a121152db9705b56670bdb1e
I534922 <jordan.levin@sap.com>
2023-09-10 20:14:43 -0700

    init repo
    Signed-off-by: I534922 <jordan.levin@sap.com>

---
+++ config/provider.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2021 Upbound Inc.
+*/
+
+package config
+
+import (
+	// Note(turkenh): we are importing this to embed provider schema document
+	_ "embed"
+
+	ujconfig "github.com/upbound/upjet/pkg/config"
+)
+
+const (
+	resourcePrefix = "confluent"
+	modulePath     = "github.com/crossplane-contrib/provider-confluent"
+)
+
+//go:embed schema.json
+var providerSchema string
+
+//go:embed provider-metadata.yaml
+var providerMetadata string
+
+// GetProvider returns provider configuration
+func GetProvider() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithRootGroup("crossplane.io"),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithFeaturesPackage("internal/features"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		// add custom config functions
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
