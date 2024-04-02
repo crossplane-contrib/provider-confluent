d80bfb0d0cb2b50d5ac6c0a85e9ad9c3f0d70037
HoussinSa <Houssin.saadeh@yahoo.com>
2024-04-01 17:39:39 -0700

    Adding schema registry Cluster (#14)
    * Adding schema registry Cluster
    
    Signed-off-by: I569132 <houssin.saadeh@sap.com>
    
    * Update Makefile
    
    Signed-off-by: I569132 <houssin.saadeh@sap.com>
    
    * removing up binary
    
    Signed-off-by: I569132 <houssin.saadeh@sap.com>
    
    * format go code
    
    Signed-off-by: I569132 <houssin.saadeh@sap.com>
    
    ---------
    
    Signed-off-by: I569132 <houssin.saadeh@sap.com>
    Co-authored-by: Jordan Levin <jleevinn@gmail.com>

--- config/provider.go
+++ config/provider.go
@@ -16,6 +16,7 @@ import (
 	confluentkafkacluster "github.com/crossplane-contrib/provider-confluent/config/confluent_kafka_cluster"
 	confluentkafkaclusterconfig "github.com/crossplane-contrib/provider-confluent/config/confluent_kafka_cluster_config"
 	confluentrolebinding "github.com/crossplane-contrib/provider-confluent/config/confluent_role_binding"
+	confluentschemaregistrycluster "github.com/crossplane-contrib/provider-confluent/config/confluent_schema_registry_cluster"
 	confluentserviceaccount "github.com/crossplane-contrib/provider-confluent/config/confluent_service_account"
 )
 
@@ -49,6 +50,7 @@ func GetProvider() *ujconfig.Provider {
 		confluentapikey.Configure,
 		confluentkafkaacl.Configure,
 		confluentrolebinding.Configure,
+		confluentschemaregistrycluster.Configure,
 	} {
 		configure(pc)
 	}
