f382628349e2f750b2ee096684ab284cb3038d06
Data Platform Engineering <no-reply@newday.co.uk>
2026-02-10 09:47:54 +0000

    fix: add confluent connector
    Signed-off-by: Sachin Malanki <sachin.chini@gmail.com>

--- apis/confluent/v1alpha1/zz_generated_terraformed.go
+++ apis/confluent/v1alpha1/zz_generated_terraformed.go
@@ -101,6 +101,90 @@ func (tr *APIKey) GetTerraformSchemaVersion() int {
 	return 0
 }
 
+// GetTerraformResourceType returns Terraform resource type for this Connector
+func (mg *Connector) GetTerraformResourceType() string {
+	return "confluent_connector"
+}
+
+// GetConnectionDetailsMapping for this Connector
+func (tr *Connector) GetConnectionDetailsMapping() map[string]string {
+	return map[string]string{"config_sensitive": "spec.forProvider.configSensitiveSecretRef"}
+}
+
+// GetObservation of this Connector
+func (tr *Connector) GetObservation() (map[string]any, error) {
+	o, err := json.TFParser.Marshal(tr.Status.AtProvider)
+	if err != nil {
+		return nil, err
+	}
+	base := map[string]any{}
+	return base, json.TFParser.Unmarshal(o, &base)
+}
+
+// SetObservation for this Connector
+func (tr *Connector) SetObservation(obs map[string]any) error {
+	p, err := json.TFParser.Marshal(obs)
+	if err != nil {
+		return err
+	}
+	return json.TFParser.Unmarshal(p, &tr.Status.AtProvider)
+}
+
+// GetID returns ID of underlying Terraform resource of this Connector
+func (tr *Connector) GetID() string {
+	if tr.Status.AtProvider.ID == nil {
+		return ""
+	}
+	return *tr.Status.AtProvider.ID
+}
+
+// GetParameters of this Connector
+func (tr *Connector) GetParameters() (map[string]any, error) {
+	p, err := json.TFParser.Marshal(tr.Spec.ForProvider)
+	if err != nil {
+		return nil, err
+	}
+	base := map[string]any{}
+	return base, json.TFParser.Unmarshal(p, &base)
+}
+
+// SetParameters for this Connector
+func (tr *Connector) SetParameters(params map[string]any) error {
+	p, err := json.TFParser.Marshal(params)
+	if err != nil {
+		return err
+	}
+	return json.TFParser.Unmarshal(p, &tr.Spec.ForProvider)
+}
+
+// GetInitParameters of this Connector
+func (tr *Connector) GetInitParameters() (map[string]any, error) {
+	p, err := json.TFParser.Marshal(tr.Spec.InitProvider)
+	if err != nil {
+		return nil, err
+	}
+	base := map[string]any{}
+	return base, json.TFParser.Unmarshal(p, &base)
+}
+
+// LateInitialize this Connector using its observed tfState.
+// returns True if there are any spec changes for the resource.
+func (tr *Connector) LateInitialize(attrs []byte) (bool, error) {
+	params := &ConnectorParameters{}
+	if err := json.TFParser.Unmarshal(attrs, params); err != nil {
+		return false, errors.Wrap(err, "failed to unmarshal Terraform state parameters for late-initialization")
+	}
+	opts := []resource.GenericLateInitializerOption{resource.WithZeroValueJSONOmitEmptyFilter(resource.CNameWildcard)}
+
+	li := resource.NewGenericLateInitializer(opts...)
+	return li.LateInitialize(&tr.Spec.ForProvider, params)
+}
+
+// GetTerraformSchemaVersion returns the associated Terraform schema version
+func (tr *Connector) GetTerraformSchemaVersion() int {
+	return 0
+}
+
 // GetTerraformResourceType returns Terraform resource type for this Environment
 func (mg *Environment) GetTerraformResourceType() string {
 	return "confluent_environment"
